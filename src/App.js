b17392f0fab262a4ef546a58a846bc15db564547
liuzhaohe <liuzhaohe@xueqiu.com>
2021-06-21 21:32:35 +0800

    feat: add FileSearch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FileSearch from './components/FileSearch';
 
 function App() {
   return (
     <div className="App container-fluid">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>this is left</h1>
+        <div className="col-3 left-panel">
+          <FileSearch title="我的策略"
+          onFileSearch={() => {}} />
         </div>
         <div className="col-9 bg-primary right-panel">
           <h1>this is right</h1>
---
+++ src/components/FileSearch.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const FileSearch = ({ title, onFileSearch }) => {
+  const [inputActive, setInputActive] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="alert alert-primary">
+      {!inputActive && (
+        <div className="d-flex justify-content-between align-items-center">
+          <span>{title}</span>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => {
+              setInputActive(true);
+            }}
+          >
+            搜索
+          </button>
+        </div>
+      )}
+      {inputActive && (
+        <div className="row d-flex">
+          <input className="col-8" value={value} onChange={(e) => { setValue(e.target.value) }}></input>
+          <button
+            type="button"
+            className="btn btn-primary col-4"
+            onClick={() => {
+              setInputActive(false);
+            }}
+          >
+            关闭
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileSearch;
