56c8bf547e95060a002e1b759a44cb02255aa5f5
liuzhaohe <liuzhaohe@xueqiu.com>
2021-07-16 09:56:21 +0800

    chore: 移除old husky & 简化commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,16 +6,14 @@ module.exports = {
       2,
       "always",
       [
-        "bug", // 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
-        "feat", // 新功能（feature）
-        "fix", // 修补bug
-        "docs", // 文档（documentation）
-        "style", // 格式（不影响代码运行的变动）
+        "feat",
+        "fix",
+        "docs",
+        "style",
         "refactor", // 重构（即不是新增功能，也不是修改bug的代码变动）
-        "test", // 增加测试
+        "test",
         "chore", // 构建过程或辅助工具的变动
         "revert", // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
-        "merge", // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
       ],
     ]
   }
